9898474a2eb2c9c85c1eaf643423cd37a0ad5a17
Hellrockerz <mukulgautam18112000@gmail.com>
2025-03-27 18:47:58 +0530

    added app constants

--- Utils/appConstants.js
+++ Utils/appConstants.js
@@ -1,4 +1,11 @@
-const BASE_URL = 'http://localhost:5000/api'
-const IMAGE_URL = 'http://localhost:5000/images'
+//*************************LIVE VERCEL****************************/
+
+const BASE_URL = 'https://file-explorer-backend.vercel.app/api'
+const IMAGE_URL = 'https://file-explorer-backend.vercel.app/images'
+
+//*************************LOCAL****************************/
+
+// const BASE_URL = 'http://localhost:5000/api'
+// const IMAGE_URL = 'http://localhost:5000/images'
 
 module.exports = { BASE_URL, IMAGE_URL }
